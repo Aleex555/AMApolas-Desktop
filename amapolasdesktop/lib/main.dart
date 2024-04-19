46fdac516f201554f9bb9539b7f8f8e9d12d96a3
alex <amartinezgonzalez.cf@iesesteveterradas.cat>
2024-04-19 16:50:24 +0200

    .

--- amapolasdesktop/lib/main.dart
+++ amapolasdesktop/lib/main.dart
@@ -26,6 +26,7 @@ class LanguagePage extends StatefulWidget {
 
 class _LanguagePageState extends State<LanguagePage> {
   List<String> _languages = [];
+  String? _selectedLanguage;
 
   @override
   void initState() {
@@ -34,14 +35,19 @@ class _LanguagePageState extends State<LanguagePage> {
   }
 
   _fetchLanguages() async {
-    var url = Uri.parse('http://localhost:3000/languages');
-    var response = await http.get(url);
-    if (response.statusCode == 200) {
-      setState(() {
-        _languages = List<String>.from(json.decode(response.body));
-      });
-    } else {
-      // Handle error
+    var url = Uri.parse('http://localhost:3000/api/languages');
+    try {
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        setState(() {
+          _languages = List<String>.from(json.decode(response.body));
+        });
+      } else {
+        print("Error loading languages: ${response.statusCode}");
+        print("Response body: ${response.body}");
+      }
+    } catch (e) {
+      print("Exception caught: $e");
     }
   }
 
@@ -51,20 +57,31 @@ class _LanguagePageState extends State<LanguagePage> {
       appBar: AppBar(
         title: Text('Select a Language'),
       ),
-      body: ListView.builder(
-        itemCount: _languages.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(_languages[index]),
-            onTap: () {
-              Navigator.push(
+      body: Column(
+        children: [
+          if (_languages.isNotEmpty)
+            DropdownButton<String>(
+              value: _selectedLanguage,
+              hint: Text("Choose a language"),
+              items: _languages.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  _selectedLanguage = value;
+                });
+                Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) =>
-                          WordPage(language: _languages[index])));
-            },
-          );
-        },
+                    builder: (context) => WordPage(language: value!),
+                  ),
+                );
+              },
+            ),
+        ],
       ),
     );
   }
@@ -81,21 +98,28 @@ class WordPage extends StatefulWidget {
 
 class _WordPageState extends State<WordPage> {
   List<String> _words = [];
+  String? _selectedLetter;
 
   @override
   void initState() {
     super.initState();
-    _fetchWords();
   }
 
-  _fetchWords() async {
-    var url = Uri.parse('http://localhost:3000/words/${widget.language}');
-    var response = await http.get(url);
-    if (response.statusCode == 200) {
-      setState(() {
-        _words = List<String>.from(json.decode(response.body));
-      });
-    } else {}
+  void _fetchWords(String letter) async {
+    var url =
+        Uri.parse('http://localhost:3000/api/words/${widget.language}/$letter');
+    try {
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        setState(() {
+          _words = List<String>.from(json.decode(response.body));
+        });
+      } else {
+        print('Failed to load words with status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print("Exception caught: $e");
+    }
   }
 
   @override
@@ -104,13 +128,37 @@ class _WordPageState extends State<WordPage> {
       appBar: AppBar(
         title: Text('Words in ${widget.language}'),
       ),
-      body: ListView.builder(
-        itemCount: _words.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(_words[index]),
-          );
-        },
+      body: Column(
+        children: [
+          DropdownButton<String>(
+            value: _selectedLetter,
+            hint: Text("Choose a letter"),
+            items: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map((String letter) {
+              return DropdownMenuItem<String>(
+                value: letter,
+                child: Text(letter),
+              );
+            }).toList(),
+            onChanged: (value) {
+              setState(() {
+                _selectedLetter = value;
+              });
+              if (value != null) {
+                _fetchWords(value);
+              }
+            },
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _words.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_words[index]),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
